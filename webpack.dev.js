89dc4195822423f96eadba9c4d55d8b0224902a1
Yazan nazzal <yazan.ham14@gmail.com>
2024-08-31 21:42:07 +0300

    creating the dist file and setup webpack

---
+++ webpack.dev.js
@@ -0,0 +1,45 @@
+const webpack = require("webpack"),
+  htmlWebpackPlugin = require("html-webpack-plugin"),
+  { CleanWebpackPlugin } = require("clean-webpack-plugin"),
+  path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: ["./src/client/index.js"],
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "var",
+    library: "Client",
+    clean: true,
+  },
+  optimization: {
+    minimize: true,
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+    new CleanWebpackPlugin({
+      dry: true,
+      verbose: false,
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
+    }),
+  ],
+};
---
+++ webpack.prod.js
@@ -0,0 +1,51 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"),
+  TerserPlugin = require("terser-webpack-plugin"),
+  htmlWebpackPlugin = require("html-webpack-plugin"),
+  { CleanWebpackPlugin } = require("clean-webpack-plugin"),
+  CssMinimizerPlugin = require("css-minimizer-webpack-plugin"),
+  path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: ["./src/client/index.js"],
+  devtool: "hidden-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  output: {
+    filename: "bundle.[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "var",
+    library: "Client",
+    clean: true,
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.[contenthash].css",
+    }),
+    new htmlWebpackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+    new CleanWebpackPlugin({
+      dry: true,
+      verbose: false,
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
+    }),
+  ],
+};
